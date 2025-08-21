2a675f9826b881afd436fabb69f698093763cd49
hellogaon <la_vega_@naver.com>
2025-08-21 21:57:51 +0900

    Add LOG_PATH

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -2,6 +2,7 @@
 # Please change the BASE_PATH to your actual project directory
 export BASE_PATH=`pwd`/scratch
 export LGF_PATH="${BASE_PATH}/LGF"
+export LOG_PATH="${BASE_PATH}/LOG"
 export SHARD_SIZE=3072
 
 if [ ! -d "$BASE_PATH" ]; then
