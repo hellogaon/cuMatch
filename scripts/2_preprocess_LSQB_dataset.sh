03e3046dba47528134286199454d4544396c994c
hellogaon <la_vega_@naver.com>
2025-08-21 21:53:18 +0900

    Edit scripts name

---
+++ scripts/2_preprocess_LSQB_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. ./scripts/vars.sh
+
+if [ ! -d "${LSQB_RELABELED_DATASET_PATH}" ]; then
+    mkdir -p "${LSQB_RELABELED_DATASET_PATH}"
+fi
+
+./build/csv_relabeler ${LSQB_DATASET_PATH} ${LSQB_RELABELED_DATASET_PATH} ${LSQB_SCHEMA_FILE} y
