2a817bb568ae832477bc31a48fa743ae9e8dcaf3
hellogaon <la_vega_@naver.com>
2025-08-21 21:57:12 +0900

    Add all benchmark

---
+++ scripts/clean_start_all.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. ./scripts/vars.sh
+
+SFs=( 0.1 )
+# SFs=( 0.1 1 10 100 1000 )
+
+shopt -s nullglob
+files=("${BASE_PATH}"/*)
+
+non_gitkeep_files=()
+for f in "${files[@]}"; do
+    if [ "$(basename "$f")" != ".gitkeep" ]; then
+        non_gitkeep_files+=("$f")
+    fi
+done
+
+if [ ${#non_gitkeep_files[@]} -ne 0 ]; then
+    echo "Directory BASE_PATH=${BASE_PATH} is not empty."
+    echo "Please manually clean it before running this script."
+    exit 1
+fi
+
+if [ ! -d "${LOG_PATH}" ]; then
+    mkdir -p "${LOG_PATH}"
+fi
+
+for SF in ${SFs[@]}; do
+  export SF=$SF
+  . ./scripts/vars.sh
+
+  ./scripts/1_download_LSQB_dataset.sh 2>&1 | tee "${LOG_PATH}/1_sf${SF}.log"
+  ./scripts/2_preprocess_LSQB_dataset.sh 2>&1 | tee "${LOG_PATH}/2_sf${SF}.log"
+  ./scripts/3_generate_LSQB_LGF.sh 2>&1 | tee "${LOG_PATH}/3_sf${SF}.log"
+  ./scripts/4_run_LSQB_benchmark.sh 2>&1 | tee "${LOG_PATH}/4_sf${SF}.log"
+
+done
