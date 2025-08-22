2186637a617ceb035bdb449f3198dfe48f4a8c4d
hellogaon <la_vega_@naver.com>
2025-08-22 23:18:53 +0900

    Edit BASE_PATH

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -1,6 +1,6 @@
 # Common Parameters
 # Please change the BASE_PATH to your actual project directory
-export BASE_PATH=/mnt/disk1/swpark/test
+export BASE_PATH=`pwd`/scratch
 export LGF_PATH="${BASE_PATH}/LGF"
 export LOG_PATH="${BASE_PATH}/log"
 
